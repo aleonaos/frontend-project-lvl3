b2a6703c8993f5e185b344ecf360cad00eb6e89b
unknown <aleonaosotova@yandex.ru>
2021-11-22 15:24:06 +0300

    add validate.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import 'bootstrap/dist/css/bootstrap.css';
 import validate from './validate';
 
 console.log('Hello World!');
+console.log(validate('hello', []));
--- src/validate.js
+++ src/validate.js
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
-export default (address) => {
+export default (address, validUrls) => {
   const schema = yup.object().shape({
-    url: yup.string().url(),
+    url: yup.string().url('invalid').notOneOf(validUrls, 'doubleUrl'),
   });
 
   return schema.validate({ url: address });
